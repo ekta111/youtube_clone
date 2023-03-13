cc71f9bcceea3c5a7e1de87302f3c38a4b055676
ekta111 <ekta.aitech@gmail.com>
2023-03-13 14:37:46 +0530

    resolving

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -12,6 +12,7 @@ const Feed = () => {
     }, []);
 
     return (
+        
         <div className="flex flex-row h-[calc(100%-56px)]">
             <LeftNav />
             <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
